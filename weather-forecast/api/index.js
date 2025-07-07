92bbc2af7879162e6aa6508d6c18aa9099aa820d
Sangeetha <sangeethagopal94@gmail.com>
2025-07-07 12:16:21 +0530

    vercel config

--- weather-forecast/api/index.js
+++ weather-forecast/api/index.js
@@ -1,5 +1,10 @@
-const path = require('path');
-
-const serverDistPath = path.join(process.cwd(), 'dist/weather-forecast/server/server.mjs');
-
-export default import(serverDistPath).then(module => module.app);
+import { app } from '../dist/weather-forecast/server/server.mjs'; // Adjust the path as needed
+    
+    export default async (req, res) => {
+      try {
+        app(req, res);
+      } catch (error) {
+        console.error('Error handling request:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    };
